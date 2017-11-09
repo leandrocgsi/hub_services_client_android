58b37f17da0cd052a8d3d12f18ea9bc4d79873bd
leandrocgsi <leandrocgsi@gmail.com>
2017-11-09 13:53:04 -0200

    Implementing menus

--- app/src/main/java/br/com/erudio/simplecrud/activities/MainActivity.java
+++ app/src/main/java/br/com/erudio/simplecrud/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.erudio.simplecrud.activities;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,97 +22,37 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView textViewResponse;
-
-    private EditText editTextName;
-    private EditText editTextCpf;
-    private EditText editTextBirthday;
-
-    private Button buttonSubmit;
-
-    private PessoaFisicaAPIService api;
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fisical_person);
-
-        editTextName = (EditText) findViewById(R.id.et_name);
-        editTextCpf = (EditText) findViewById(R.id.et_cpf);
-        editTextBirthday = (EditText) findViewById(R.id.et_birthday);
-
-        Button buttonSubmit = (Button) findViewById(R.id.btn_submit);
-        textViewResponse = (TextView) findViewById(R.id.tv_response);
-
-        api = ApiUtils.getAPIService();
-
-        buttonSubmit.setOnClickListener(this);
+        setContentView(R.layout.activity_main);
     }
-
-    private void insertPerson(){
-
-        String name = editTextName.getText().toString().trim();
-        String cpf = editTextCpf.getText().toString().trim();
-        Date birthday = new Date();//editTextBirthday.getText().toString().trim();
-
-        PessoaFisica pessoaFisica = new PessoaFisica();
-        pessoaFisica.setNomeNomeFantasia(name);
-        pessoaFisica.setCpfcnpj(cpf);
-        pessoaFisica.setDataDeNascimento("1975-12-05");
-
-        api.savePerson(pessoaFisica).enqueue(new Callback<PessoaFisica>() {
-
-            @Override
-            public void onResponse(Call<PessoaFisica> call, Response<PessoaFisica> response) {
-                if(response.isSuccessful()) {
-                    showResponse(response.body().toString());
-                    Log.i(TAG, "pessoaFisica submitted to API." + response.body().toString());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<PessoaFisica> call, Throwable t) {
-                showErrorMessage();
-                Log.e(TAG, "Unable to submit pessoaFisica to API.");
-            }
-        });
-    }
-
-    /*private void getAllPessoaFisicas() {
-        Call<List<PessoaFisica>> getAllPessoaFisicasCall = api.getPessoas();
-
-        getAllPessoaFisicasCall.enqueue(new Callback<List<PessoaFisica>>() {
-            @Override
-            public void onResponse(Call<List<PessoaFisica>> call, Response<List<PessoaFisica>> response) {
-                displayPessoaFisica(response.body().get(0));
-            }
-
-            @Override
-            public void onFailure(Call<List<PessoaFisica>> call, Throwable t) {
-                Log.e(TAG, "Error occured while fetching pessoaFisica.");
-            }
-        });
-    }*/
 
 
     @Override
-    public void onClick(View view) {
-        insertPerson();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    public void showResponse(String response) {
-        if(textViewResponse.getVisibility() == View.GONE) {
-            textViewResponse.setVisibility(View.VISIBLE);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.pessoa_fisica_menu:
+                Toast.makeText(getApplicationContext(),"Item 1 Selected",Toast.LENGTH_LONG).show();
+                return true;
+
+            case R.id.pessoa_juridca_menu:
+                Toast.makeText(getApplicationContext(),"Item 2 Selected",Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        textViewResponse.setText(response);
-    }
-
-    public void showErrorMessage() {
-        Toast.makeText(this, R.string.mssg_error_saving_fisical_person, Toast.LENGTH_SHORT).show();
     }
 }
+
 //SEE: https://www.simplifiedcoding.net/retrofit-android-example/           and             http://swarajsaaj.github.io/posts/consuming-rest-api-using-retrofit-android
 //https://www.concretepage.com/android/android-options-menu-example-using-getmenuinflater-inflate-oncreateoptionsmenu-and-onoptionsitemselected
