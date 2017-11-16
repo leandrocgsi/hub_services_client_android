bc70d009ee6e050823da28eb497f0a3ca5d7e7ed
leandrocgsi <leandrocgsi@gmail.com>
2017-11-16 15:41:59 -0200

    Refactoring save and update

--- app/src/main/java/br/com/erudio/simplecrud/activities/LegalPersonActivity.java
+++ app/src/main/java/br/com/erudio/simplecrud/activities/LegalPersonActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class LegalPersonActivity extends Activity implements View.OnClickListener {
+public class LegalPersonActivity extends AppCompatActivity {
 
     private static final String TAG = LegalPersonActivity.class.getSimpleName();
     private TextView textViewResponse;
@@ -31,11 +32,11 @@ public class LegalPersonActivity extends Activity implements View.OnClickListene
     private EditText editTextCnpj;
     private EditText editTradeName;
 
-    ProgressDialog pd;
-
-    private Button buttonSubmit;
+    private LegalPerson legalPerson = new LegalPerson();
 
     private LegalPersonAPIService api;
+
+    ProgressDialog pd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,109 +51,109 @@ public class LegalPersonActivity extends Activity implements View.OnClickListene
         Button buttonUpdate = (Button) findViewById(R.id.btn_update);
         textViewResponse = (TextView) findViewById(R.id.tv_response);
 
-        pd = new ProgressDialog(this);
-
-        String companyName = editTextCompanyName.getText().toString().trim();
-        String tradeName = editTradeName.getText().toString().trim();
-        String cnpj = editTextCnpj.getText().toString().trim();
-
-        final LegalPerson legalPerson = new LegalPerson();
-        legalPerson.setNameTradeName(tradeName);
-        legalPerson.setCompanyName(companyName);
-        legalPerson.setCpfcnpj(cnpj);
-
         api = ApiUtils.getLegalPersonAPIService();
 
         Intent data = getIntent();
         final String iddata = data.getStringExtra("id");
-        if(iddata == null) {
-
-            buttonSave.setVisibility(View.VISIBLE);
-            buttonUpdate.setVisibility(View.GONE);
-
-            buttonSave.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pd.setMessage("send data ... ");
-                    pd.setCancelable(false);
-                    pd.show();
-
-                    api.savePerson(legalPerson).enqueue(new Callback<LegalPerson>() {
-
-                        @Override
-                        public void onResponse(Call<LegalPerson> call, Response<LegalPerson> response) {
-                            if(response.isSuccessful()) {
-                                showResponse(response.body().toString());
-                                Log.i(TAG, "legalPerson submitted to API." + response.body().toString());
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<LegalPerson> call, Throwable t) {
-                            showErrorMessage();
-                            Log.e(TAG, "Unable to submit legalPerson to API.");
-                        }
-                    });
-                }
-            });
-        } else {
+        if(iddata != null) {
             buttonSave.setVisibility(View.GONE);
             buttonUpdate.setVisibility(View.VISIBLE);
-
-            legalPerson.setId(Long.valueOf(iddata));
 
             editTextCompanyName.setText(data.getStringExtra("et_company_name"));
             editTradeName.setText(data.getStringExtra("et_trading_name"));
             editTextCnpj.setText(data.getStringExtra("et_cnpj"));
-
-            buttonUpdate.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    pd.setMessage("update ....");
-                    pd.setCancelable(false);
-                    pd.show();
-
-                    api.updatePerson(legalPerson.getId(), legalPerson).enqueue(new Callback<LegalPerson>() {
-
-                        @Override
-                        public void onResponse(Call<LegalPerson> call, Response<LegalPerson> response) {
-                            if(response.isSuccessful()) {
-                                showResponse(response.body().toString());
-                                Log.i(TAG, "legalPerson submitted to API." + response.body().toString());
-                            }
-                        }
-
-                        @Override
-                        public void onFailure(Call<LegalPerson> call, Throwable t) {
-                            showErrorMessage();
-                            Log.e(TAG, "Unable to submit legalPerson to API.");
-                        }
-                    });
-                }
-            });
-
         }
-    }
 
-    @Override
-    public void onClick(View view) {
-        //TODO
-    }
+        pd = new ProgressDialog(this);
 
-    public void returnToMain(View view) {
-        //setContentView(R.layout.activity_main);
-
-        int timeout = 50;
-        Timer t2 = new Timer();
-        t2.schedule(new TimerTask() {
-
+        /*buttonDelete.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                finish();
-                Intent homepage = new Intent(LegalPersonActivity.this, MainActivity.class);
-                startActivity(homepage);
+            public void onClick(View v) {
+                pd.setMessage("Loading Hapus ...");
+                pd.setCancelable(false);
+                pd.show();
+
+                Call<LegalPerson> del  = api.deletePerson(Long.valueOf(iddata));
+                del.enqueue(new Callback<LegalPerson>() {
+                    @Override
+                    public void onResponse(Call<LegalPerson> call, Response<LegalPerson> response) {
+                        //TODO
+                    }
+
+                    @Override
+                    public void onFailure(Call<LegalPerson> call, Throwable t) {
+                        pd.hide();
+                        Log.d("Retro", "onFailure");
+                    }
+                });
             }
-        }, timeout);
+        });*/
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                api.updatePerson(legalPerson.getId(), legalPerson).enqueue(new Callback<LegalPerson>() {
+
+                    @Override
+                    public void onResponse(Call<LegalPerson> call, Response<LegalPerson> response) {
+                        if(response.isSuccessful()) {
+                            showResponse(response.body().toString());
+                            Log.i(TAG, "legalPerson submitted to API." + response.body().toString());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<LegalPerson> call, Throwable t) {
+                        showErrorMessage();
+                        Log.e(TAG, "Unable to submit legalPerson to API.");
+                    }
+                });
+            }
+        });
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pd.setMessage("send data ... ");
+                pd.setCancelable(false);
+                pd.show();
+
+                String companyName = editTextCompanyName.getText().toString().trim();
+                String tradeName = editTradeName.getText().toString().trim();
+                String cnpj = editTextCnpj.getText().toString().trim();
+
+                legalPerson.setNameTradeName(tradeName);
+                legalPerson.setCompanyName(companyName);
+                legalPerson.setCpfcnpj(cnpj);
+
+                buttonSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        pd.setMessage("send data ... ");
+                        pd.setCancelable(false);
+                        pd.show();
+
+                        api.savePerson(legalPerson).enqueue(new Callback<LegalPerson>() {
+
+                            @Override
+                            public void onResponse(Call<LegalPerson> call, Response<LegalPerson> response) {
+                                if(response.isSuccessful()) {
+                                    showResponse(response.body().toString());
+                                    Log.i(TAG, "legalPerson submitted to API." + response.body().toString());
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<LegalPerson> call, Throwable t) {
+                                showErrorMessage();
+                                Log.e(TAG, "Unable to submit legalPerson to API.");
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
     public void showResponse(String response) {
